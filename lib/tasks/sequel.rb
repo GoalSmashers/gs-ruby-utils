9d853f797ca83daf24653a43fbbb022a55953388
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2012-05-25 09:38:43 +0200

    Added sequel task.

---
+++ lib/tasks/sequel.rb
@@ -0,0 +1,26 @@
+require 'rake'
+require 'rake/tasklib'
+require 'yaml'
+
+module GS::Rake
+  class SequelTask < TaskLib
+    def define
+      namespace :db do
+        desc 'Run database migrations'
+        task :migrate do
+          system("sequel -m db/migrations -e #{ENV['RACK_ENV'] || 'development'} -E config/database.yml")
+        end
+
+        desc 'Rollback last database migration'
+        task :rollback do
+          config = YAML.load_file('config/database.yml')
+          env = ENV['RACK_ENV'] || 'development'
+          database = config[env]['database']
+          target_version = open("|psql #{database} -U goalsmashers -c 'select version from schema_info' -t -A").gets.to_i - 1
+
+          system("sequel -m db/migrations -M #{target_version} -e #{env} -E config/database.yml")
+        end
+      end
+    end
+  end
+end
