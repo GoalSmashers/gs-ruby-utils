943f7e3476941c38aec5171378962a7c6b8ed0e5
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2012-06-05 10:28:24 +0200

    Added generic mailer.

---
+++ lib/shared_gs/mail/generic_mailer.rb
@@ -0,0 +1,32 @@
+class GenericMailer < MailProxy
+  def self.from_name
+    'Dummy Name'
+  end
+
+  def self.from_address
+    'generic@address.com'
+  end
+
+  def self.path
+    'some_path'
+  end
+
+  def self.deliver(template, ctx = {})
+    mailer = self.new
+    method = template.to_s.split('/').first.to_sym
+    mail_fields = mailer.method(method).call(ctx).merge(
+      from: "#{from_name} <#{from_address}>",
+      sent_on: Time.now.to_s,
+      charset: 'UTF-8',
+      content_transfer_encoding: "8bit"
+    )
+    mail_fields[:to] = ctx[:to] || from_address unless mail_fields[:to]
+    if ctx[:subject]
+      mail_fields[:subject] = ctx[:subject]
+    else
+      ctx[:subject] = mail_fields[:subject]
+    end
+
+    mailer.deliver_email("#{path}/#{template}", mail_fields, ctx, mail_fields[:to].kind_of?(Array))
+  end
+end
---
+++ test/mail/generic_mailer_test.rb
@@ -0,0 +1,80 @@
+require 'test_helper'
+
+class GenericMailer
+  def demo(ctx = {})
+    { subject: "Generic Subject" }
+  end
+end
+
+describe GenericMailer do
+  include FlexMock::TestCase
+
+  describe 'deliver' do
+    it 'should handle default options' do
+      verify_mail_fields = lambda do |args|
+        args.length == 6 && \
+          args[:to] == GenericMailer.from_address && \
+          args[:from] == "#{GenericMailer.from_name} <#{GenericMailer.from_address}>" && \
+          args[:charset] == 'UTF-8' && \
+          args[:sent_on] != nil && \
+          args[:content_transfer_encoding] == '8bit'
+      end
+
+      verify_ctx = lambda do |args|
+        args.length == 1 && args[:subject] == "Generic Subject"
+      end
+
+      flexmock(GenericMailer)
+        .new_instances
+        .should_receive(:deliver_email)
+        .with('some_path/demo', on { |args| verify_mail_fields.call(args) }, on { |args| verify_ctx.call(args) }, false)
+        .once
+        .and_return(true)
+
+      GenericMailer.deliver(:demo)
+    end
+
+    it 'should add to option' do
+      to = Sham.email
+
+      flexmock(GenericMailer)
+        .new_instances
+        .should_receive(:deliver_email)
+        .with('some_path/demo', on { |args| args[:to] == to }, any, false)
+        .once
+        .and_return(true)
+
+      GenericMailer.deliver(:demo, { to: to })
+    end
+
+    it 'should pass context' do
+      ctx = {
+        to: Sham.email,
+        x1: 1,
+        x2: 5
+      }
+
+      flexmock(GenericMailer)
+        .new_instances
+        .should_receive(:deliver_email)
+        .with('some_path/demo', on { |args| args[:to] == ctx[:to] }, on { |args| args[:x1] == 1 && args[:x2] == 5 }, false)
+        .once
+        .and_return(true)
+
+      GenericMailer.deliver(:demo, ctx)
+    end
+
+    it 'should send bulk emails' do
+      to = [Sham.email, Sham.email]
+
+      flexmock(GenericMailer)
+        .new_instances
+        .should_receive(:deliver_email)
+        .with('some_path/demo', any, any, true)
+        .once
+        .and_return(true)
+
+      GenericMailer.deliver(:demo, { to: to })
+    end
+  end
+end
