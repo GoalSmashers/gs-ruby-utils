01c90196de8c6bf57f8c798cd6f02030909ef2d8
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2012-06-07 14:35:54 +0200

    Added more mail test helpers.

--- lib/shared_gs/tests/mail_test_helpers.rb
+++ lib/shared_gs/tests/mail_test_helpers.rb
@@ -1,3 +1,5 @@
+require 'mail'
+
 module GS::Tests
   module MailTestHelpers
     def body_has?(email, text)
@@ -9,5 +11,14 @@ module GS::Tests
       assert_equal(false, email.text_part.body.include?(text), text)
       assert_equal(false, email.html_part.body.include?(text), text)
     end
+  end
+end
+
+Mail.defaults do
+  delivery_method :test
+end
+
+module Mail
+  def self.bulk_deliver(messages)
   end
 end
