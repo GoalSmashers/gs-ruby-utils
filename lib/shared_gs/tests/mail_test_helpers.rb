b19d6bc0b37284d964bcb71d15c645db13c94847
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2012-06-07 14:26:18 +0200

    Made mail test helpers into GS::Tests namespace.

--- lib/shared_gs/tests/mail_test_helpers.rb
+++ lib/shared_gs/tests/mail_test_helpers.rb
@@ -1,11 +1,13 @@
-module MailTestHelpers
-  def body_has?(email, text)
-    assert_equal(true, email.text_part.body.include?(text), text)
-    assert_equal(true, email.html_part.body.include?(text), text)
-  end
+module GS::Tests
+  module MailTestHelpers
+    def body_has?(email, text)
+      assert_equal(true, email.text_part.body.include?(text), text)
+      assert_equal(true, email.html_part.body.include?(text), text)
+    end
 
-  def body_has_no?(email, text)
-    assert_equal(false, email.text_part.body.include?(text), text)
-    assert_equal(false, email.html_part.body.include?(text), text)
+    def body_has_no?(email, text)
+      assert_equal(false, email.text_part.body.include?(text), text)
+      assert_equal(false, email.html_part.body.include?(text), text)
+    end
   end
 end
