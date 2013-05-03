a2c593c7303b005973bc4d5243744f38e6013bed
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2013-05-03 23:11:06 +0200

    Experimental ago/since login for months.

--- lib/shared_gs/core/fixnum.rb
+++ lib/shared_gs/core/fixnum.rb
@@ -40,7 +40,9 @@ class Fixnum
   end
 
   def month
-    30 * 24 * 3600 * self
+    new_value = 30 * 24 * 3600 * self
+    new_value.instance_variable_set('@_months_diff', self)
+    new_value
   end
 
   def months
@@ -55,15 +57,49 @@ class Fixnum
     year
   end
 
-  def ago
-    Time.zone.now - self
+  def ago(from = Time.zone.now)
+    _months_diff ?
+      months_difference(from, -1) :
+      from - self
   end
 
-  def since
-    Time.zone.now + self
+  def since(from = Time.zone.now)
+    _months_diff ?
+      months_difference(from, 1) :
+      from + self
   end
 
   def from_now
     since
+  end
+
+  private
+
+  attr_reader :_months_diff
+
+  def months_difference(from, direction)
+    to_year = from.year + ((from.month + direction * _months_diff - 1) / 12.0).floor
+    to_leap_year = to_year % 4 == 0 && to_year % 100 != 0
+
+    to_month = from.month + direction * (_months_diff % 12)
+    to_month += 12 if to_month < 1
+    to_month -= 12 if to_month > 12
+
+    to_day = case from.day
+    when 30..31
+      to_day = if [4, 6, 9, 11].include?(to_month)
+        30
+      elsif to_month == 2
+        to_leap_year ? 29 : 28
+      else
+        from.day
+      end
+    when 29
+      to_month == 2 && !to_leap_year ? 28 : from.day
+    else
+      from.day
+    end
+
+    Time.zone.local(to_year, to_month, to_day, from.hour, from.min, from.sec)
   end
 end
--- test/core/fixnum_test.rb
+++ test/core/fixnum_test.rb
@@ -58,8 +58,145 @@ describe Fixnum do
         5.minutes.since.must_equal (Time.now + 5.minutes)
         2.hours.since.must_equal (Time.now + 2.hours)
         1.day.since.must_equal (Time.now + 1.day)
-        2.months.since.must_equal (Time.now + 2.months)
         4.years.since.must_equal (Time.now + 4.years)
+      end
+    end
+  end
+
+  describe 'ago from date' do
+    it 'should calculate ago from date' do
+      1.month.ago(Time.zone.local(2014,1,1)).must_equal Time.zone.local(2013,12,1)
+    end
+  end
+
+  describe 'since from date' do
+    it 'should calculate since from date' do
+      12.month.since(Time.zone.local(2014,1,1)).must_equal Time.zone.local(2015,1,1)
+    end
+  end
+
+  describe 'ago in months' do
+    describe 'no trimming' do
+      it 'should be ok for 31st' do
+        Timecop.freeze(Time.zone.local(2013,1,31)) do
+          1.month.ago.must_equal Time.zone.local(2012,12,31)
+        end
+      end
+
+      it 'should be ok for 30th' do
+        Timecop.freeze(Time.zone.local(2013,1,30)) do
+          3.months.ago.must_equal Time.zone.local(2012,10,30)
+        end
+      end
+
+      it 'should be ok for 29th' do
+        Timecop.freeze(Time.zone.local(2012,2,29)) do
+          5.months.ago.must_equal Time.zone.local(2011,9,29)
+        end
+      end
+
+      it 'should be ok for 28th' do
+        Timecop.freeze(Time.zone.local(2012,2,28)) do
+          15.months.ago.must_equal Time.zone.local(2010,11,28)
+        end
+      end
+
+      it 'should be ok for 1st' do
+        Timecop.freeze(Time.zone.local(2012,2,1)) do
+          1.month.ago.must_equal Time.zone.local(2012,1,1)
+        end
+      end
+    end
+
+    describe 'trimming' do
+      it 'should trim from 31st to 30th' do
+        Timecop.freeze(Time.zone.local(2013,5,31)) do
+          1.month.ago.must_equal Time.zone.local(2013,4,30)
+        end
+      end
+
+      it 'should trim from 31st to 29th' do
+        Timecop.freeze(Time.zone.local(2012,5,31)) do
+          3.months.ago.must_equal Time.zone.local(2012,2,29)
+        end
+      end
+
+      it 'should trim from 31st to 28th' do
+        Timecop.freeze(Time.zone.local(2013,5,31)) do
+          3.months.ago.must_equal Time.zone.local(2013,2,28)
+        end
+      end
+
+      it 'should trim from 29th to 28th' do
+        Timecop.freeze(Time.zone.local(2012,2,29)) do
+          12.months.ago.must_equal Time.zone.local(2011,2,28)
+        end
+      end
+
+      it 'should trim from 31st to 28th in non leap year' do
+        Timecop.freeze(Time.zone.local(2003,3,31)) do
+          37.months.ago.must_equal Time.zone.local(2000,2,28)
+        end
+      end
+    end
+  end
+
+  describe 'since in months' do
+    describe 'no trimming' do
+      it 'should be ok for 31st' do
+        Timecop.freeze(Time.zone.local(2013,5,31)) do
+          3.months.since.must_equal Time.zone.local(2013,8,31)
+        end
+      end
+
+      it 'should be ok for 30th' do
+        Timecop.freeze(Time.zone.local(2013,4,30)) do
+          2.months.since.must_equal Time.zone.local(2013,6,30)
+        end
+      end
+
+      it 'should be ok for 29th' do
+        Timecop.freeze(Time.zone.local(2012,2,29)) do
+          5.months.since.must_equal Time.zone.local(2012,7,29)
+        end
+      end
+
+      it 'should be ok for 28th' do
+        Timecop.freeze(Time.zone.local(2012,2,28)) do
+          15.months.since.must_equal Time.zone.local(2013,5,28)
+        end
+      end
+
+      it 'should be ok for 1st' do
+        Timecop.freeze(Time.zone.local(2012,2,1)) do
+          18.months.ago.must_equal Time.zone.local(2010,8,1)
+        end
+      end
+    end
+
+    describe 'trimming' do
+      it 'should trim from 31st to 30th' do
+        Timecop.freeze(Time.zone.local(2013,5,31)) do
+          1.month.since.must_equal Time.zone.local(2013,6,30)
+        end
+      end
+
+      it 'should trim from 31st to 29th' do
+        Timecop.freeze(Time.zone.local(2011,12,31)) do
+          2.months.since.must_equal Time.zone.local(2012,2,29)
+        end
+      end
+
+      it 'should trim from 31st to 28th' do
+        Timecop.freeze(Time.zone.local(2012,12,31)) do
+          2.months.since.must_equal Time.zone.local(2013,2,28)
+        end
+      end
+
+      it 'should trim from 29th to 28th' do
+        Timecop.freeze(Time.zone.local(2012,2,29)) do
+          12.months.since.must_equal Time.zone.local(2013,2,28)
+        end
       end
     end
   end
