0c11c330e6e48f87ebbe16001264e29bc60b2eed
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2013-10-08 20:52:38 +0200

    Adds MocksHelper.

---
+++ lib/shared_gs/specs/mocks_helper.rb
@@ -0,0 +1,28 @@
+module GS
+  module Specs
+    module MocksHelper
+      include FlexMock::TestCase
+
+      def unmock(scope)
+        if scope == :all
+          flexmock_teardown
+        else
+          scope.try(:flexmock_teardown)
+        end
+      end
+
+      def mock(*args, &block)
+        flexmock(*args) do |mock|
+          yield(mock) if block_given?
+        end
+      end
+
+      alias_method :double, :mock
+
+      def remock(object)
+        unmock(object)
+        mock(object)
+      end
+    end
+  end
+end
